10f042e016a6ea4f984a9249f5218002be4760eb
Mathias Sescau <ssm4431a@ens.fsi>
2023-09-28 09:42:22 +0200

    TP1 en cours

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,10 +8,80 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
-
+	
+	//classe interne Marche
+	
+	private static class Marche{
+		private Etal[] etals;
+		
+		private Marche(int nbEtals) {
+			etals = new Etal[nbEtals];
+			for(int i = 0; i<nbEtals; i++) {
+				etals[i] = new Etal();
+			}
+		}
+		
+		private void utiliserEtal(int indiceEtal,Gaulois vendeur,String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur,produit,nbProduit);
+		}
+		
+		private int trouverEtalLibre() {
+			for(int i = 0; i<etals.length; i++) {
+				if(!etals[i].isEtalOccupe()){
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		private Etal[] trouverEtals(String produit) {
+			int nbEtals = etals.length;
+			Etal[] etalsAvecProduit;
+			etalsAvecProduit = new Etal[nbEtals];
+			int indiceAvecProd = 0;
+			
+			for(int i = 0; i<nbEtals; i++) {
+				if(etals[i].contientProduit(produit)){
+					etalsAvecProduit[indiceAvecProd] = etals[i];
+					indiceAvecProd ++;
+				}
+			}
+			return etalsAvecProduit;
+		}
+		
+		private Etal trouverVendeur(Gaulois gaulois) {
+			
+			for(int i = 0; i<etals.length; i++) {
+				if(etals[i].getVendeur() == gaulois){
+					return etals[i];
+				}
+			}
+			return null;
+		}
+		
+		private String afficherMarche() {
+			StringBuilder chaine = new StringBuilder();
+			int nbEtalVide = 0;
+			for(int i = 0; i<etals.length; i++) {
+				if(etals[i].isEtalOccupe()) {
+					chaine.append(etals[i].afficherEtal());
+				}else {
+					nbEtalVide ++;
+				}
+			}
+			if(nbEtalVide > 0) {
+				chaine.append("Il reste " + nbEtalVide + " étals non utilisés dans le marché.\n");
+			}
+			return chaine.toString();
+		}
+		
+	}
+	
+	
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		Marche marche = new Marche(10);
 	}
 
 	public String getNom() {
